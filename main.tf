ca45153c946b5cd88cb7db3426619bd318541bd9
Filipe Revez <filipevrevez@outlook.com>
2024-06-12 14:54:54 +0100

    chore: Update Redis deployment and service configuration

--- main.tf
+++ main.tf
@@ -83,8 +83,8 @@ output "result" {
     }
     // UCP resource IDs
     resources = [
-        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata[0].namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
-        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata[0].namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
+        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata[0].namespace}/providers/core/Service/${kubernetes_service.redis.metadata[0].name}",
+        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata[0].namespace}/providers/apps/Deployment/${kubernetes_deployment.redis.metadata[0].name}"
     ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
